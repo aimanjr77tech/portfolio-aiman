ff151c6056e011a75006cfa68c7f1f5df479bb0b
aimanjr77tech <f1online7@gmail.com>
2025-11-27 01:05:35 +0100

    hola

--- config/settings.py
+++ config/settings.py
@@ -162,16 +162,7 @@ DATABASES = {
 }
 
 
-import dj_database_url
 
-DATABASE_URL = config("DATABASE_URL", default=None)
-
-if DATABASE_URL:
-    DATABASES["default"] = dj_database_url.config(
-        default=DATABASE_URL,
-        conn_max_age=600,
-        ssl_require=True
-    )
 
 # Si más adelante usas Postgres en Render:
 # DATABASES["default"] = dj_database_url.config(
